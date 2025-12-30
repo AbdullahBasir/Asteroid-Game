37ea506ea265345e9b429fea05587eace2a127f3
abdullahbasir <abdullah.sbasir@gmail.com>
2025-12-30 12:41:17 +0300

    B: added movement to the player WASD

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
+from player import *
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -13,7 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        updatable.update(dt)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
